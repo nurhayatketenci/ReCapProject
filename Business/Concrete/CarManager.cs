41b6e2edd67f4be9b5354bc55aafdbb7b2be878b
Nurhayat <69644512+nurhayatketenci@users.noreply.github.com>
2021-02-27 19:32:11 +0300

    12-1

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,81 +1,83 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Business.Concrete
 {
-    
-    
-        public class CarManager : ICarService
+
+
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
         {
-            ICarDal _carDal;
+            _carDal = carDal;
+        }
 
-            public CarManager(ICarDal carDal)
-            {
-                _carDal = carDal;
-            }
+        [ValidationAspect(typeof(CarValidator))]
+        public IResult Add(Car car)
+        {
+           
+            _carDal.Add(car);
+            return new SuccessResult(Messages.ProductAdded);
+        }
 
 
-            public IResult Add(Car car)
-            {
-                if (!(car.Description.Length >= 2 && car.DailyPrice > 0))
-                {
-                    return new ErrorResult(Messages.ProductNameInvalid);
-                }
-                _carDal.Add(car);
-                return new SuccessResult(Messages.ProductAdded);
-            }
 
-        
 
         public IResult Delete(Car car)
-            {
-                _carDal.Delete(car);
-                return new SuccessResult(Messages.ProductDelete);
-            }
-            public IResult Update(Car car)
-            {
-                _carDal.Update(car);
-                return new SuccessResult(Messages.ProductUpdate);
-            }
+        {
+            _carDal.Delete(car);
+            return new SuccessResult(Messages.ProductDelete);
+        }
+        public IResult Update(Car car)
+        {
+            _carDal.Update(car);
+            return new SuccessResult(Messages.ProductUpdate);
+        }
 
-            public IDataResult<List<Car>> GetAll()
-            {
-            if (DateTime.Now.Hour==23)
+        public IDataResult<List<Car>> GetAll()
+        {
+            if (DateTime.Now.Hour == 23)
             {
                 return new ErrorDataResult<List<Car>>(Messages.MaintenanceTime);
             }
-            return new SuccessDataResult<List<Car>>(_carDal.GetAll(),Messages.ProductListed);
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.ProductListed);
         }
 
-            public IDataResult<Car> GetCarById(int carId)
-            {
-                return new SuccessDataResult<Car>(_carDal.Get(p => p.CarId == carId));
-            }
+        public IDataResult<Car> GetCarById(int carId)
+        {
+            return new SuccessDataResult<Car>(_carDal.Get(p => p.CarId == carId));
+        }
 
-            public IDataResult<List<Car>> GetCarsByBrandId(int brandId)
-            {
-                return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.BrandId == brandId));
-            }
+        public IDataResult<List<Car>> GetCarsByBrandId(int brandId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.BrandId == brandId));
+        }
 
-            public IDataResult<List<Car>> GetCarsByColorId(int colarId)
-            {
-                return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColorId == colarId));
-            }
-            public IDataResult<List<CarDetailDto>> GetCarDetails()
-            {
-                return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
-            }
-            public IDataResult<List<Car>> GetAllByModelYear(int year)
-            {
-                return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ModelYear == year));
-            }
+        public IDataResult<List<Car>> GetCarsByColorId(int colarId)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(p => p.ColorId == colarId));
+        }
+        public IDataResult<List<CarDetailDto>> GetCarDetails()
+        {
+            return new SuccessDataResult<List<CarDetailDto>>(_carDal.GetCarDetails());
+        }
+        public IDataResult<List<Car>> GetAllByModelYear(int year)
+        {
+            return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ModelYear == year));
         }
     }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -27,7 +27,6 @@ namespace WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -52,7 +51,7 @@ namespace WebAPI
 
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
